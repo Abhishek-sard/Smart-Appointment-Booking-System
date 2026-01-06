0e455bb85a387a879d4725b8245cb99b3c2075fb
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-01-06 21:14:56 +0545

    updated

---
+++ Server/models/Booking.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+const bookingSchema = new mongoose.Schema(
+    {
+        user: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+        date: String,
+        time: String,
+        status:{
+            type: String,
+            enum: ["pending", "approved", "rejected"],
+            default: "pending"
+        }
+    },
+    {timestamps: true}
+);
+
+export default mongoose.model("Booking", bookingSchema);
