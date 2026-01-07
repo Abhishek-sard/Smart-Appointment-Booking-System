09ef1d107394607b624fbf300529f946c7d266be
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-01-07 11:50:33 +0545

    updated

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 
+import authRoutes from "./routes/authRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+
 dotenv.config();
 connectDB();
 
@@ -14,6 +17,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/bookings", bookingRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
